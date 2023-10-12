1e4bf042acfd55875d6704faefd35f2e297718a1
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-12 15:18:24 +0200

    build: define absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  base: './',
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+      components: path.resolve(__dirname, './src/components'),
+      pages: path.resolve(__dirname, './src/pages')
+    }
+  }
+});
