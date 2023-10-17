2431dd2c9d813914e949c2e5b62180c7d5282984
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-17 18:02:14 +0200

    style: change vanta background color

--- src/data/ThemeProperties/dark.ts
+++ src/data/ThemeProperties/dark.ts
@@ -4,7 +4,7 @@ const dark: CustomPaletteOptions = {
   background: {
     default: '#171c22'
   },
-  VantaBackgroundColor: '#171c22'
+  VantaBackgroundColor: '#0c0f12'
 } as CustomPaletteOptions;
 
 export default dark;
