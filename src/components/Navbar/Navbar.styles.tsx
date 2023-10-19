e0d828923dfb43c0647b0e0bb437df100e01bdfd
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-19 17:10:18 +0200

    remove: unused theme interface import

--- src/components/Navbar/Navbar.styles.tsx
+++ src/components/Navbar/Navbar.styles.tsx
@@ -1,12 +1,17 @@
 import { AppBar, Toolbar, styled } from '@mui/material';
 
-import type CustomPaletteOptions from 'data/ThemeProperties/CustomPalette.interface';
-
-export const StyledToolbar = styled(Toolbar)(({ theme }) => ({}));
+export const StyledToolbar = styled(Toolbar)(() => ({
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '80%'
+}));
 
 export const StyledAppbar = styled(AppBar)(({ theme }) => ({
-  background: (theme.palette as CustomPaletteOptions).AppbarBackground,
+  alignItems: 'center',
+  background: theme.palette.AppbarBackground,
   backdropFilter: 'blur(10px)',
   WebkitBackdropFilter: 'blur(10px)',
-  zIndex: (theme.palette as CustomPaletteOptions).NavbarZindex
+  zIndex: theme.palette.NavbarZindex,
+  paddingTop: 10,
+  paddingBottom: 10
 }));
