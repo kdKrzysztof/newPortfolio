fe08579f6ec5015e1b273b654241289a5edc36e2
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-19 17:44:06 +0200

    feat: add sx prop to ChangeThemeButton

--- src/components/ChangeThemeButton/ChangeThemeButton.tsx
+++ src/components/ChangeThemeButton/ChangeThemeButton.tsx
@@ -1,21 +1,24 @@
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { IconButton, Tooltip } from '@mui/material';
+import type { SxProps, Theme } from '@mui/material';
 
 import { useAppDispatch, useAppSelector } from 'hooks/reduxHooks/reduxHooks';
 
 import { toggleTheme } from 'store/reducers/themeSlice';
 
-const ChangeThemeButton = () => {
+interface IChangeThemeButton {
+  edge?: false | 'end' | 'start' | undefined;
+  sx?: SxProps<Theme>;
+}
+
+const ChangeThemeButton = ({ edge, sx }: IChangeThemeButton) => {
   const themeColor = useAppSelector((state) => state.theme);
   const dispatch = useAppDispatch();
 
   return (
     <Tooltip title="Change theme">
-      <IconButton
-        onClick={() => dispatch(toggleTheme())}
-        color="inherit"
-        sx={{ position: 'absolute', right: '1%', zIndex: '1' }}>
+      <IconButton onClick={() => dispatch(toggleTheme())} color="inherit" edge={edge} sx={sx}>
         {themeColor === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
       </IconButton>
     </Tooltip>
