150ecdabf32b42be4dc1ee655f4297c2dd39a4c6
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-19 17:39:37 +0200

    fix: custom typography prop declaration

--- src/theme.d.ts
+++ src/theme.d.ts
@@ -3,24 +3,28 @@ import type { PaletteOptions, Theme, ThemeOptions } from '@mui/material/styles/'
 
 declare module '@mui/material/styles' {
   interface PaletteOptions {
-    typography: {
-      navLinks: {
-        fontSize: React.CSSProperties['fontSize'];
-      };
-    };
+    NavbarZindex?: number;
+    AppbarBackground?: React.CSSProperties['backgroundColor'];
+    VantaBackgroundColor?: React.CSSProperties['backgroundColor'];
+  }
+
+  interface Palette {
     NavbarZindex: number;
     AppbarBackground: React.CSSProperties['backgroundColor'];
     VantaBackgroundColor: React.CSSProperties['backgroundColor'];
   }
 
-  interface Palette {
-    typography?: {
-      navLinks?: {
-        fontSize?: React.CSSProperties['fontSize'];
-      };
-    };
-    NavbarZindex?: number;
-    AppbarBackground?: React.CSSProperties['backgroundColor'];
-    VantaBackgroundColor?: React.CSSProperties['backgroundColor'];
+  interface TypographyVariants {
+    navLinks: React.CSSProperties;
+  }
+
+  interface TypographyVariantsOptions {
+    navLinks?: React.CSSProperties;
+  }
+}
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    navLinks: true;
   }
 }
