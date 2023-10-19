2427553ce2121ed6dabb3224b36126e04ae69c1c
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-19 17:38:54 +0200

    style: add primary.main color

--- src/data/ThemeProperties/dark.ts
+++ src/data/ThemeProperties/dark.ts
@@ -4,6 +4,9 @@ const dark: PaletteOptions = {
   background: {
     default: '#171c22'
   },
+  primary: {
+    main: 'rgb(0, 173, 181)'
+  },
   NavbarZindex: 2,
   AppbarBackground: 'rgba(12, 15, 18, .25)',
   VantaBackgroundColor: '#0c0f12'
--- src/data/ThemeProperties/light.ts
+++ src/data/ThemeProperties/light.ts
@@ -4,6 +4,9 @@ const light: PaletteOptions = {
   background: {
     default: 'rgba(255, 255, 255)'
   },
+  primary: {
+    main: 'rgb(0, 173, 181)'
+  },
   NavbarZindex: 2,
   AppbarBackground: 'rgba(12, 15, 18, .25)',
   VantaBackgroundColor: 'rgb(155, 155, 155)'
