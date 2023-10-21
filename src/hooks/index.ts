2116442a74989a7285f96ac39614e182db650002
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-10-21 14:49:05 +0200

    feat: add window size hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export { default as useCustomTheme } from './MUITheme';
+export { default as useGetWindowSize } from './useGetWindowSize';
---
+++ src/hooks/useGetWindowSize.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+const useGetWindowSize = () => {
+  const [windowWidth, setWidth] = useState(window.innerWidth);
+  const [windowHeight, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWidth(window.innerWidth)
+      setHeight(window.innerHeight)
+    };
+    window.addEventListener('resize', handleWindowResize);
+  }, []);
+
+  return {windowWidth, windowHeight}
+}
+
+export default useGetWindowSize
