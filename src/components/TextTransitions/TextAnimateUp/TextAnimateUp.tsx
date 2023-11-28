4dadbfcea20f6e20d40d65981fd2fc537b905f46
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-11-28 17:57:40 +0100

    refactor: separate TextAnimateUp component logic

---
+++ src/components/TextTransitions/TextAnimateUp/TextAnimateUp.tsx
@@ -0,0 +1,32 @@
+import { Variant } from '@mui/material/styles/createTypography';
+import { motion } from 'framer-motion';
+
+import useTextAnimationUp from './TextAnimationUp.utils';
+
+interface ITextAnimateUp {
+  text: string;
+  textVariant?: Variant;
+  splitBy: 'word' | 'letter';
+  staggerTime?: number;
+}
+
+/**
+ * Staggered text animation component using Material-UI Typography and framer-motion.
+ * Each **letter** transitions from an initial state of opacity 0 and y = -20 to a visible state
+ * with opacity 1 and y = 0.
+ */
+
+const TextAnimateUp = ({ text, textVariant, splitBy, staggerTime }: ITextAnimateUp) => {
+  const { wordAnimation, letterAnimation } = useTextAnimationUp({ text, textVariant });
+
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      transition={staggerTime ? { staggerChildren: staggerTime } : { staggerChildren: 0.1 }}>
+      {splitBy === 'word' ? wordAnimation() : letterAnimation()}
+    </motion.div>
+  );
+};
+
+export default TextAnimateUp;
