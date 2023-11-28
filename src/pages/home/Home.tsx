2eb55c67be0c498b0477f609dc8d00b3db1f09d3
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-11-28 17:55:42 +0100

    refactor: change TextAnimateUp import

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,8 +2,7 @@ import { Button } from '@mui/material';
 import { Suspense, lazy } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import HomeCharacter from '/HomeCharacter.svg';
-import { TextAnimateUpWord } from 'src/components/TextTransitions';
+import { TextAnimateUp } from 'src/components/TextTransitions';
 
 import {
   HomeButtonContainer,
@@ -13,6 +12,7 @@ import {
   HomeGraphics,
   HomeMainTitle
 } from './Home.styles';
+import HomeCharacter from '/HomeCharacter.svg';
 
 const VantaBackground = lazy(() => import('components/VantaBackground'));
 
@@ -25,7 +25,7 @@ const Home = () => {
         <HomeContent>
           <HomeDescContainer>
             <HomeMainTitle variant="h3">{t('MainTitle')}</HomeMainTitle>
-            <TextAnimateUpWord text={t('MainDesc')} textVariant="h5" />
+            <TextAnimateUp text={t('MainDesc')} textVariant="h5" splitBy="word" />
             <HomeButtonContainer>
               <Button variant="contained">About me</Button>
               <Button variant="outlined">Contact with me</Button>
