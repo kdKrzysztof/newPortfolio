45249690b2fc7eb05832b29ac8096a658b8f9bdd
kdKrzysztof <57269917+kdKrzysztof@users.noreply.github.com>
2023-12-15 19:21:24 +0100

    feat: add home image loop animation

--- src/pages/home/framer/animation.tsx
+++ src/pages/home/framer/animation.tsx
@@ -1,8 +1,31 @@
-export const defaultAnimation = {
+import type { Variants } from 'framer-motion';
+
+export const defaultAnimation: Variants = {
   visible: {
     opacity: 1
   },
   hidden: {
     opacity: 0
+  }
+};
+
+export const imageAnimation: Variants = {
+  initial: {
+    opacity: 0,
+    y: 0
+  },
+  default: {
+    y: 20,
+    opacity: 1,
+    transition: {
+      duration: 3,
+      repeatType: 'mirror',
+      repeat: Infinity,
+      ease: 'easeInOut',
+      opacity: {
+        duration: 1,
+        repeat: 0
+      }
+    }
   }
 };
