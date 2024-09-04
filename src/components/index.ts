57cee7d32a62300224e9be49b0c32f2ca9d058a2
kdKrzysztof <57269917+kdKrzysztof@users.noreply.github.com>
2024-09-04 10:19:12 +0200

    feat: init Project template component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as LoadingLogo } from './LoadingLogo';
 export { default as Navbar } from './Navbar';
 export { default as ChangeLanguageButton } from './ChangeLanguageButton';
 export { default as ChangeThemeButton } from './ChangeThemeButton';
+export { default as Project } from './Project';
+export { default as Skills } from './Skills';
